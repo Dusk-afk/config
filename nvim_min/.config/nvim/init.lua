9cd0f9a0d56877b80dc4909cb8a1d9768a070d6b
Piyush <piyushsvps@gmail.com>
2026-06-01 00:17:27 +0530

    nvim min basic syntax highlighting

--- nvim_min/.config/nvim/init.lua
+++ nvim_min/.config/nvim/init.lua
@@ -99,3 +99,6 @@ vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
 
+-- syntax highlighting
+vim.opt.syntax = "on"
+vim.filetype.enable()
